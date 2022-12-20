6a054f482071619cc1703be158ca281b1207797c
Edward-kwabena-Twumasi <45181928+Edward-kwabena-Twumasi@users.noreply.github.com>
2022-12-20 22:43:23 +0000

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -14,7 +14,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("employees");
+        _db = db.db("notes");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
